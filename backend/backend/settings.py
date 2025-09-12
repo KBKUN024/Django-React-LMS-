d462995e795da583408cd2284fc628ef9fc01c4c
KBKUN024 <944412796@qq.com>
2025-09-12 21:52:30 +0800

    添加dockerfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gpq1qn#va)#oje@0g8vwxjzjj-byze09(**2l^&*y2*1bs#ex*"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
