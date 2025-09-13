3e32992216ddaad5940e1c5e5dd4fe00372fc457
KBKUN024 <944412796@qq.com>
2025-09-13 20:23:43 +0800

    测试push

--- frontend/frontend/src/views/partials/BaseHeader.tsx
+++ frontend/frontend/src/views/partials/BaseHeader.tsx
@@ -44,7 +44,7 @@ function BaseHeader() {
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          Tyuan
+          T-yuan
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
