3c8752e3bd133cc70b6e89d0f1167cf5a6e85b97
KBKUN024 <944412796@qq.com>
2025-09-14 19:51:19 +0800

    test again

--- frontend/frontend/src/views/partials/BaseHeader.tsx
+++ frontend/frontend/src/views/partials/BaseHeader.tsx
@@ -44,7 +44,7 @@ function BaseHeader() {
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          tyuan1935
+          tyuan1951
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
